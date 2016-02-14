ad83a800ecc80e029a112fbe7f51a47f22f5b4dd
Александр Чаусов <traktoro_13@mail.ru>
2016-02-14 15:35:31 +0300

    добавлены всплывашки

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
@@ -16,7 +16,11 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        oldName = session.get('name')
+        if oldName is not None and oldName != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form = form, name = session.get('name'))
 
