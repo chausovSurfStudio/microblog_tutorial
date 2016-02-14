bab8a1f0c4fdcc7b22f33a1c38e3124bfe736e93
Александр Чаусов <traktoro_13@mail.ru>
2016-02-14 15:16:13 +0300

    использование redirect для post запросов

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
@@ -14,12 +14,11 @@ class NameForm(Form):
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
