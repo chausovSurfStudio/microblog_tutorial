775270a5a370448fecb1d5602c0c9902312671c7
Александр Чаусов <traktoro_13@mail.ru>
2016-02-20 21:57:40 +0300

    в структуру таблицы users добавлены новые поля, обновлена бд

--- app/model.py
+++ app/model.py
@@ -1,21 +1,34 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class Role(db.Model):
-	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(64), unique = True)
-	users = db.relationship('User', backref = 'role')
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    users = db.relationship('User', backref = 'role')
 
-	def __rept__(self):
-		return '<Role %r' % self.name
+    def __rept__(self):
+        return '<Role %r' % self.name
 
 class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key = True)
-	username = db.Column(db.String(64), unique = True, index = True)
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-	def __rept__(self):
-		return '<User %r' % self.username
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @property
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __rept__(self):
+        return '<User %r' % self.username
