0bcd7a437d933ccc6bbd477b3f93e8de4402ab97
Александр Чаусов <traktoro_13@mail.ru>
2016-02-22 12:49:28 +0300

    в бд добавлено поле к users, новые методы для подтверждения регистрации

--- app/model.py
+++ app/model.py
@@ -2,6 +2,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 db = SQLAlchemy()
 
@@ -21,6 +23,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique = True, index = True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default = False)
 
     @property
     def password(self):
@@ -35,6 +38,22 @@ class User(UserMixin, db.Model):
 
     def __rept__(self):
         return '<User %r' % self.username
+
+    def generate_confirmation_token(self, expiration = 3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm' : self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @login_manager.user_loader
     def load_user(user_id):
