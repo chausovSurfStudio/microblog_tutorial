6e9550e80ded84e7c75436bd77d3b2a9cffb24ca
Chausov Alexander <chausov@surfstudio.ru>
2016-02-27 12:38:22 +0300

    в БД реализованы роли пользователей

--- app/model.py
+++ app/model.py
@@ -11,10 +11,28 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-    users = db.relationship('User', backref = 'role')
+    default = db.Column(db.Boolean, default = True, index = True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
     def __rept__(self):
         return '<Role %r' % self.name
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+        'User': (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES, True),
+        'Moderator': (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES | Permission.MODERATE_COMMENTS, False),
+        'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name = r).first()
+            if role is None:
+                role = Role(name = r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -58,3 +76,10 @@ class User(UserMixin, db.Model):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
