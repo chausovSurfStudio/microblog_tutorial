aa2a077180277b531b4d1c9921b56cc955e890d6
Александр Чаусов <traktoro_13@mail.ru>
2016-03-02 20:23:41 +0300

    посты выводятся на странице профиля

--- app/views.py
+++ app/views.py
@@ -98,7 +98,8 @@ def user(username):
     user = User.query.filter_by(username = username).first()
     if user is None:
         abort(404)
-    return render_template('user.html', user = user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user = user, posts = posts)
 
 @app.route('/edit_profile', methods = ['GET', 'POST'])
 @login_required
